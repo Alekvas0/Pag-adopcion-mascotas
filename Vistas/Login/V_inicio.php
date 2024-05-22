0132372fc77b6a70cfc14832373688aba84ad05a
Alekvas0 <alekvas03@gmail.com>
2024-05-22 13:51:44 -0400

    Vistas paginas principales en html

--- Vistas/Login/V_inicio.php
+++ Vistas/Login/V_inicio.php
@@ -12,7 +12,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
 
-    <link rel="stylesheet" href="../Estilos/styles.css"/>
+    <!-- <link rel="stylesheet" href="../Estilos/styles.css"/> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     <!-- Navigation -->
@@ -58,7 +60,8 @@
     </div>    
     
 
-    <script>src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"</script>
+    <!-- <script>src="../../node_modules/bootstrap/dist/js/bootstrap.min.js"</script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
